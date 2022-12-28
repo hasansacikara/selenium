96ab0856c68683a617307212c4afd0871ecc1b40
hasansacikara <sacikara33@gmail.com>
2022-12-28 23:09:42 +0300

    8.gün sonu HandingWindows

---
+++ src/test/java/day08_HandlingWindows/C04_TestBaseIlkTest.java
@@ -0,0 +1,19 @@
+package day08_HandlingWindows;
+
+import Utilities.ReusableMethods;
+import Utilities.TestBase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class C04_TestBaseIlkTest extends TestBase {
+    @Test
+    public void test01(){
+        // amazon'a gidin
+        driver.get("https://www.amazon.com");
+        // amazon'a gittiginizi test edin
+        String expectedKelime= "amazon";
+        String actualUrl= driver.getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains(expectedKelime));
+        ReusableMethods.bekle(3);
+    }
+}
